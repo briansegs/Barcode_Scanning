de77be414a7116d1e7eb25dd3b05b8b5f04dbfd6
bsegers <briansegs@gmail.com>
2023-11-15 21:56:41 -0500

    add: WebCam

---
+++ camera.py
@@ -0,0 +1,31 @@
+"""Setup camera"""
+import cv2 as cv
+
+class WebCam:
+    """Webcam"""
+    def __init__(self, closeKey='q'):
+        self.closeKey = closeKey
+
+    def showCloseKey(self):
+        """prints the key that closes the camera to the terminal"""
+        print(self.closeKey)
+
+    def setCloseKey(self, newKey):
+        """sets the key that closes the webcam"""
+        self.closeKey = newKey
+
+    def startCam(self):
+        """starts the webcam"""
+        cam = cv.VideoCapture(0)
+
+        while True:
+            ret, frame = cam.read()
+
+            cv.imshow('frame', frame)
+
+            if cv.waitKey(1) == ord(self.closeKey):
+                break
+
+        cam.release()
+
+        cv.destroyAllWindows()
