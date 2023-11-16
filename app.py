5f7815d84c9869fb4766b0891637c681102b26f7
bsegers <briansegs@gmail.com>
2023-11-16 01:13:23 -0500

    add locations

---
+++ app.py
@@ -0,0 +1,23 @@
+"""App"""
+from datetime import datetime
+
+now = datetime.now()
+date = now.strftime("%m/%d/%Y")
+time = now.strftime("%H:%M:%S")
+
+states = {
+    'ny' : 'New York',
+    'nj' : 'New Jersey',
+    'pa' : 'Pennsylvinia',
+    'fl' : 'Florida'
+}
+
+location = ''
+while location not in states:
+    location = input('Enter state: ')
+    if location in states:
+        location = states[location]
+        break
+
+print(location)
+        
