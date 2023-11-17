9c37083261aea7e038a7cd6cda840306b795aaea
bsegers <briansegs@gmail.com>
2023-11-17 10:37:49 -0500

    refactor: Put getLocation into a function

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 """App"""
 from datetime import datetime
 from data import states
+from scanner import Scanner
 
 now = datetime.now()
 date = now.strftime("%m/%d/%Y")
 time = now.strftime("%H:%M:%S")
 
-location = ''
-while location not in states:
-    location = input('Enter state: ')
-    if location in states:
-        location = states[location]
-        break
-    print('Not a state \nStates: ny, nj, pa, fl \nPlease pick one from the list')
-print(location)
-        
+def getLocation():
+    """Retuns location from input statment"""
+    location = ''
+    while location not in states:
+        location = input('Enter state: ')
+        if location in states:
+            location = states[location]
+            break
+        print('Not a state \nStates: ny, nj, pa, fl \nPlease pick one from the list')
+    print(f'Locantion: {location}')
+    return location
+
+scan = Scanner(getLocation())
+
+print(f'Data: {scan.startScanner()}')
