a32b4fd5c2a3433fc99ec7dd75b765f575150c9b
bsegers <briansegs@gmail.com>
2023-11-21 20:29:37 -0500

    add: Test db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """App"""
+import sqlite3
 from data import getLocation
 from scanner import Scanner
 
@@ -6,11 +7,27 @@ from scanner import Scanner
 # TODO: check if scanned data is in inventory
 # TODO: add scanned data to database
 
-scan = Scanner(getLocation())
+# TODO: put getLocation() into scanner class
+# scan = Scanner(getLocation())
 
-scan.startScanner()
+# scan.startScanner()
 
-for value in scan.getData().values():
-    print("----    ----")
-    for k, v in value.items():
-        print(f'{k}: {v}')
+# for value in scan.getData().values():
+#     print("----    ----")
+#     for k, v in value.items():
+#         print(f'{k}: {v}')
+
+conn = sqlite3.connect('testDb.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+    DROP TABLE IF EXISTS Items;
+    
+    CREATE TABLE Items (
+        "barcode" INTEGER,
+        "bar type" TEXT,
+        "pickup location" TEXT,
+        "pickup date" NUMERIC,
+        "pickup time" NUMERIC
+    );
+''')
