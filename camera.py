2437a1420cb78e71729e0761c036987520baaf8e
bsegers <briansegs@gmail.com>
2023-11-27 22:13:15 -0500

    add: Scanner base code

--- camera.py
+++ camera.py
@@ -1,34 +1,21 @@
-"""camera"""
-import cv2 as cv
+"""Scanner"""
 
 # TODO: Rename file to ...
 # TODO: Rename and make into parent class
-# TODO: parent name == Scanner
-class WebCam:
-    """Webcam"""
-    def __init__(self, closeKey='q'):
-        self.closeKey = closeKey
+class Scanner:
+    """Scans Barcodes"""
+    def __init__(self):
+        self.closeKey = 'q'
+        self.data = {}
 
-    def showCloseKey(self):
-        """prints the key that closes the camera to the terminal"""
-        print(self.closeKey)
+    def getCloseKey(self):
+        "returns the key that closes the camera"
+        return self.closeKey
 
     def setCloseKey(self, newKey):
-        """sets the key that closes the webcam"""
+        "sets the key that closes the webcam"
         self.closeKey = newKey
 
-    def startCam(self):
-        """starts the webcam"""
-        cam = cv.VideoCapture(0)
-
-        while True:
-            ret, frame = cam.read()
-
-            cv.imshow('frame', frame)
-
-            if cv.waitKey(1) == ord(self.closeKey):
-                break
-
-        cam.release()
-
-        cv.destroyAllWindows()
+    def getData(self):
+        "returns stored data"
+        return self.data
