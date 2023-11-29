54e7c549b8a434119fa90315ff42563fd2ffcb40
bsegers <briansegs@gmail.com>
2023-11-28 21:06:04 -0500

    refactor: Rename files

--- scanner.py
+++ scanner.py
@@ -1,12 +1,7 @@
 """Scanner"""
-import time as t
-import cv2 as cv
-from pyzbar.pyzbar import decode
-from functions import getDate, getTime
 
-# TODO: make parent scanner class in another file and import
-# TODO: children should be for scanning 1. items and 2. agent barcodes
-# TODO: children names ScanItems and ScanAgentId
+# TODO: Rename file to ...
+# TODO: Rename and make into parent class
 class Scanner:
     """Scans Barcodes"""
     def __init__(self):
@@ -24,42 +19,3 @@ class Scanner:
     def getData(self):
         "returns stored data"
         return self.data
-
-    # TODO: should be added to child class when created
-    # TODO: seperate out to make scanner generic
-    def startScanner(self):
-        "Starts the scanner"
-
-        # look into class inheritance.
-        # look into storing the data the same way.
-        # make what I do after what is different.
-        # I want to be able to scan agent barcodes with -
-        # the same scanner. Not have two different scanners -
-        # that share 90% of the same code.
-
-        cam = cv.VideoCapture(0)
-
-        while True:
-            ret, frame = cam.read()
-
-            cv.imshow('frame', frame)
-
-            if cv.waitKey(1) == ord(self.closeKey):
-                break
-
-            for code in decode(frame):
-                bCode = code.data.decode('utf-8')
-                if bCode not in self.data:
-                    self.data[bCode] = {
-                        "barcode" : bCode,
-                        "bar_type" : code.type,
-                        "scan_date" : getDate(),
-                        "scan_time" : getTime()
-                        }
-                    print(bCode)
-                    t.sleep(5)
-                else:
-                    print(bCode)
-                    t.sleep(5)
-        cam.release()
-        cv.destroyAllWindows()
