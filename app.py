9a844a533044d956de44ece5f5f962ca599fd40e
bsegers <briansegs@gmail.com>
2023-12-21 18:19:40 -0500

    docs: Add TODO

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from functions import storeData, getLocation, getData
 # TODO: Creat agent in app if admin
 # TODO: Clean up database 
 # TODO: Plan data and how it is stored
+# TODO: Change data.json to data.py
 
 # Connects to database
 conn = sqlite3.connect('testDb.sqlite')
