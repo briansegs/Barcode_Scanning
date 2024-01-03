40d1e3848203ee1b4ff055d222f4d620dfbbc8ac
bsegers <briansegs@gmail.com>
2024-01-03 10:15:15 -0500

    docs: Add TODOs

--- drop_off_items.py
+++ drop_off_items.py
@@ -1,1 +1,8 @@
 "Drop off items"
+
+# TODO: Get agent
+# TODO: Ask if items were dropped off successfully
+# TODO: Add options
+# TODO: Yes - for each item in pending list - remove from list - add to log
+# TODO: No - "please contact HQ for futher steps" - quit
+# TODO: quit
