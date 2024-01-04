518671e28946525728e8541b8133a94e97b897f3
bsegers <briansegs@gmail.com>
2024-01-04 18:56:48 -0500

    add: Option 1

--- drop_off_items.py
+++ drop_off_items.py
@@ -1,6 +1,7 @@
 "Drop off items"
 import time as t
 from functions import getAgent
+from util import getCursorConnection, getDate, getTime
 
 # TODO: Ask if items were dropped off successfully
 # TODO: Add options
@@ -21,15 +22,51 @@ while True:
     print("Options: ")
     t.sleep(.5)
     print('''
-        1. Yes
-        2. No
-        3. Quit
+    1. Yes
+    2. No
+    3. Quit
     ''')
     t.sleep(.5)
     opt = input(">>> ")
 
     if opt == "1":
-        break
+        cur, conn = getCursorConnection()
+        getPendingDropOff = '''
+        SELECT item_id, quantity FROM pending_drop_off WHERE quantity > 0
+        '''
+        res = cur.execute(getPendingDropOff)
+        lst = res.fetchall()
+
+        insertDropOffLog = '''
+        INSERT OR IGNORE INTO drop_off_log (
+            date,
+            time,
+            quantity,
+            item_id,
+            agent_id
+        )
+        VALUES (?, ?, ?, ?, ?)
+        '''
+
+        clearPendingDropOff = '''
+        UPDATE pending_drop_off
+        SET quantity = 0
+        WHERE item_id = ?
+        '''
+
+        for i in lst:
+            date = getDate()
+            time = getTime()
+            cur.execute(insertDropOffLog,
+                (date, time, i[1], i[0], agentId))
+            cur.execute(clearPendingDropOff, (i[0],))
+
+        conn.commit()
+        conn.close()
+        t.sleep(1)
+        print("Items dropped off successfully.")
+        quit()
+
     elif opt == "2":
         t.sleep(1)
         print("please contact HQ for futher steps.")
