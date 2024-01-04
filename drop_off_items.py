62be419fdcdc73075032e69f656879fb6e614686
bsegers <briansegs@gmail.com>
2024-01-04 11:08:46 -0500

    add: Get agent

--- drop_off_items.py
+++ drop_off_items.py
@@ -1,4 +1,6 @@
 "Drop off items"
+import time as t
+from functions import getAgent
 
 # TODO: Get agent
 # TODO: Ask if items were dropped off successfully
@@ -6,3 +8,9 @@
 # TODO: Yes - for each item in pending list - remove from list - add to log
 # TODO: No - "please contact HQ for futher steps" - quit
 # TODO: quit
+
+# Login
+print('Login to start scanning.')
+t.sleep(1)
+
+agent, agentId = getAgent()
