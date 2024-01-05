d5c4d2f781956e033acc3a19c71e712841b435ab
bsegers <briansegs@gmail.com>
2024-01-05 09:34:34 -0500

    docs: Add TODOs

--- show_items.py
+++ show_items.py
@@ -1,1 +1,9 @@
 "Show items scanned"
+
+# TODO: Login agent
+# TODO: Show items scanned options:
+# TODO: All dates earliest to latest
+# TODO: All dates latest to earliest
+# TODO: Pick from dates
+# TODO: By agent
+# TODO: By location
