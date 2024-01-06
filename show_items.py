7c177c690c4bcf348a6fef9c95a67f79fe61d229
bsegers <briansegs@gmail.com>
2024-01-05 19:52:57 -0500

    add: Option 5

--- show_items.py
+++ show_items.py
@@ -1,6 +1,12 @@
 "Show items scanned"
 import time as t
-from functions import getAgent, showAllscans
+from functions import (
+    getAgent, 
+    showAllscans, 
+    getCursorConnection,
+    getLocation,
+    getScanDataFrame
+    )
 from data import getItemScanAsc, getItemScanDesc
 from util import shutDown, optionError
 
@@ -60,6 +66,48 @@ while True:
                 shutDown()
             else:
                 optionError(opt)
+
+    elif opt == "5":
+        locationId, location = getLocation()
+        t.sleep(1)
+        print("")
+        t.sleep(.5)
+
+        getItemScanlocation = '''
+            SELECT 
+            item_scan.id,
+            date,
+            time,
+            quantity,
+            item.name,
+            agent.first_name,
+            agent.last_name,
+            location.name
+            FROM item_scan
+            JOIN item JOIN agent JOIN location
+            ON 
+            item_scan.item_id = item.id 
+            AND
+            item_scan.agent_id = agent.id 
+            AND
+            item_scan.location_id = location.id
+            WHERE location_id = ? 
+            '''
+        cur, conn = getCursorConnection()
+        res = cur.execute(getItemScanlocation, (locationId,))
+        data = res.fetchall()
+        conn.close()
+
+        if len(data) > 0:
+            df = getScanDataFrame(data)
+
+            print(f'Location: {location}')
+            print("")
+            print(df)
+            print("")
+
+        else:
+            print("No data found.")
 
     elif opt == "6":
         shutDown()
