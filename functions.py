dbb4ead55aea488b8db89c0034be7cda6e84168b
bsegers <briansegs@gmail.com>
2024-01-05 20:12:33 -0500

    refactor: Move variable

--- functions.py
+++ functions.py
@@ -10,7 +10,8 @@ from data import (
     getPendingDropOff,
     insertDropOffLog,
     clearPendingDropOff,
-    getItemName
+    getItemName,
+    getItemScanlocation
     )
 from agent_scanner import AgentScanner
 
@@ -168,26 +169,6 @@ def showScansByLocation():
     print("")
     t.sleep(.5)
 
-    getItemScanlocation = '''
-        SELECT 
-        item_scan.id,
-        date,
-        time,
-        quantity,
-        item.name,
-        agent.first_name,
-        agent.last_name,
-        location.name
-        FROM item_scan
-        JOIN item JOIN agent JOIN location
-        ON 
-        item_scan.item_id = item.id 
-        AND
-        item_scan.agent_id = agent.id 
-        AND
-        item_scan.location_id = location.id
-        WHERE location_id = ? 
-        '''
     cur, conn = getCursorConnection()
     res = cur.execute(getItemScanlocation, (locationId,))
     data = res.fetchall()
