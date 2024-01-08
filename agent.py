7d607e10d39f65ed16e6d393114b96d690e57e7b
bsegers <briansegs@gmail.com>
2024-01-08 16:39:39 -0500

    add: Agent class

--- agent.py
+++ agent.py
@@ -1,1 +1,10 @@
 "agent class"
+
+class Agent:
+    "Agent"
+    def __init__(self, name, agentId, barcode, location):
+        self.name = name
+        self.id = agentId
+        self.barcode = barcode
+        self.location = location
+        
