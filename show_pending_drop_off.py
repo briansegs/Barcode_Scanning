8a2076c3d26e86758978fae0aa6278baad1a36d6
bsegers <briansegs@gmail.com>
2024-01-11 12:36:21 -0500

    add: Print pending drop off

--- show_pending_drop_off.py
+++ show_pending_drop_off.py
@@ -1,1 +1,25 @@
 "Show pending drop off"
+import pandas as pd
+from util import getCursorConnection
+
+getPendingDropOff = '''
+    SELECT
+        item.name,
+        quantity
+    FROM 
+        pending_drop_off
+    JOIN 
+        item
+    ON
+        pending_drop_off.item_id = item.id
+'''
+
+cur, conn = getCursorConnection()
+res = cur.execute(getPendingDropOff)
+data = res.fetchall()
+
+columns = ["Name", "Quantity"]
+df = pd.DataFrame(data=data, columns=columns)
+pd.set_option('display.colheader_justify', 'center')
+
+print(df)
