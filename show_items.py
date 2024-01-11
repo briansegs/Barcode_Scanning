c81e0f476d6e13a968dd27b3d806cfa7a8319685
bsegers <briansegs@gmail.com>
2024-01-10 21:57:51 -0500

    refactor: Extract function

--- show_items.py
+++ show_items.py
@@ -23,59 +23,62 @@ from util import shutDown, optionError
 # TODO: By agent / asending and desending by date
 # TODO: By location / asending and desending by date
 
-# Options
-while True:
-    print("Options: ")
-    t.sleep(.5)
-    print('''
-    1. Show all
-    2. Show by date
-    3. Show by agent
-    4. Show by location
-    5. Quit
-    ''')
-    t.sleep(.5)
-    opt = input(">>> ")
-    t.sleep(1)
-    print("")
+def showItemsScanned():
+    "Prints recorded items from the database"
 
-    if opt == "1":
-        while True:
-            print("Options: \n")
-            t.sleep(.5)
-            print("    1. Show ascending")
-            print("    2. Show descending \n")
-            t.sleep(.5)
-            opt = input(">>> ")
-            t.sleep(1)
-            print("")
-
-            if opt == "1":
-                showScans("All", getItemScansAsc, None, "Ascending")
-                break
-
-            elif opt == "2":
-                showScans("All", getItemScansDesc, None, "Descending")
-                break
-
-            else:
-                optionError(opt)
-
-    elif opt == "2":
-        date, _ = getFromList("Date", getScanDates)
-        showScans("Date", getScansByDate, date, date)
-
-    elif opt == "3":
-        agentName, agentId = getFromList("Agent", getAgents)
-        showScans("Agent",getScansByAgentId, agentId, agentName)
-
-    elif opt == "4":
-        locationName, locationId = getFromList("Location", getLocations)
-        showScans("Location", getScansBylocationId, locationId, locationName)
-
-    elif opt == "5":
-        shutDown()
-
-    else:
+    # Options
+    while True:
+        print("Options: ")
+        t.sleep(.5)
+        print('''
+        1. Show all
+        2. Show by date
+        3. Show by agent
+        4. Show by location
+        5. Quit
+        ''')
+        t.sleep(.5)
+        opt = input(">>> ")
         t.sleep(1)
-        optionError(opt)
+        print("")
+
+        if opt == "1":
+            while True:
+                print("Options: \n")
+                t.sleep(.5)
+                print("    1. Show ascending")
+                print("    2. Show descending \n")
+                t.sleep(.5)
+                opt = input(">>> ")
+                t.sleep(1)
+                print("")
+
+                if opt == "1":
+                    showScans("All", getItemScansAsc, None, "Ascending")
+                    break
+
+                elif opt == "2":
+                    showScans("All", getItemScansDesc, None, "Descending")
+                    break
+
+                else:
+                    optionError(opt)
+
+        elif opt == "2":
+            date, _ = getFromList("Date", getScanDates)
+            showScans("Date", getScansByDate, date, date)
+
+        elif opt == "3":
+            agentName, agentId = getFromList("Agent", getAgents)
+            showScans("Agent",getScansByAgentId, agentId, agentName)
+
+        elif opt == "4":
+            locationName, locationId = getFromList("Location", getLocations)
+            showScans("Location", getScansBylocationId, locationId, locationName)
+
+        elif opt == "5":
+            shutDown()
+
+        else:
+            t.sleep(1)
+            optionError(opt)
