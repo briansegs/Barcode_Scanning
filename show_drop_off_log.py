87192f4945e9a0d6bb0caaebd8f61632e771055c
bsegers <briansegs@gmail.com>
2024-01-11 23:38:24 -0500

    add: Opt 1 Ascending

--- show_drop_off_log.py
+++ show_drop_off_log.py
@@ -1,13 +1,54 @@
 "Shows drop off log"
 import time as t
 from util import getOption, shutDown, optionError
-from data import dropOffLogOptions
+from functions import showDataFrame
+from data import dropOffLogOptions, ascOrDescOptions
 
 while True:
     opt = getOption("Drop off Log options: ", dropOffLogOptions)
 
     if opt == "1":
-        pass
+        while True:
+            opt = getOption("options: ", ascOrDescOptions)
+
+            if opt == "1":
+                getDropOffLogAsc = '''
+                SELECT 
+                    drop_off_log.id,
+                    date,
+                    time,
+                    quantity,
+                    item.name,
+                    agent.first_name,
+                    agent.last_name
+                FROM 
+                    drop_off_log
+                JOIN 
+                    item 
+                JOIN 
+                    agent 
+                ON 
+                    drop_off_log.item_id = item.id 
+                AND
+                    drop_off_log.agent_id = agent.id
+                ORDER BY 
+                    date ASC, time ASC
+                '''
+
+                dropOffLogColumns = ["ID", "Date", "Time", "Quantity",
+                "Item", "Agent", "Name"]
+
+                showDataFrame(
+                    "All", 
+                    getDropOffLogAsc,
+                    None,
+                    "Ascending",
+                    dropOffLogColumns
+                )
+                break
+
+            elif opt == "2":
+                break
 
     elif opt == "2":
         pass
