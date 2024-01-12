58de8c595217381364d84019cea7da2afc276f1c
bsegers <briansegs@gmail.com>
2024-01-11 23:43:05 -0500

    refactor: Move variable

--- data.py
+++ data.py
@@ -281,11 +281,37 @@ getPendingDropOff = '''
         ON
             pending_drop_off.item_id = item.id
     '''
+    
+getDropOffLogAsc = '''
+        SELECT 
+            drop_off_log.id,
+            date,
+            time,
+            quantity,
+            item.name,
+            agent.first_name,
+            agent.last_name
+        FROM 
+            drop_off_log
+        JOIN 
+            item 
+        JOIN 
+            agent 
+        ON 
+            drop_off_log.item_id = item.id 
+        AND
+            drop_off_log.agent_id = agent.id
+        ORDER BY 
+            date ASC, time ASC
+        '''
 
 itemColumns = ["ID", "Date", "Time", "Quantity",
                 "Item", "Agent", "Name", "location"]
 
 pendingDropOffColumns = ["Name", "Quantity"]
+
+dropOffLogColumns = ["ID", "Date", "Time", "Quantity",
+                "Item", "Agent", "Name"]
 
 mainMenueOptions = {
     "1" : "Scan items for pickup",
--- show_drop_off_log.py
+++ show_drop_off_log.py
@@ -2,7 +2,12 @@
 import time as t
 from util import getOption, shutDown, optionError
 from functions import showDataFrame
-from data import dropOffLogOptions, ascOrDescOptions
+from data import (
+    dropOffLogOptions,
+    ascOrDescOptions,
+    getDropOffLogAsc,
+    dropOffLogColumns
+)
 
 while True:
     opt = getOption("Drop off Log options: ", dropOffLogOptions)
@@ -12,32 +17,6 @@ while True:
             opt = getOption("options: ", ascOrDescOptions)
 
             if opt == "1":
-                getDropOffLogAsc = '''
-                SELECT 
-                    drop_off_log.id,
-                    date,
-                    time,
-                    quantity,
-                    item.name,
-                    agent.first_name,
-                    agent.last_name
-                FROM 
-                    drop_off_log
-                JOIN 
-                    item 
-                JOIN 
-                    agent 
-                ON 
-                    drop_off_log.item_id = item.id 
-                AND
-                    drop_off_log.agent_id = agent.id
-                ORDER BY 
-                    date ASC, time ASC
-                '''
-
-                dropOffLogColumns = ["ID", "Date", "Time", "Quantity",
-                "Item", "Agent", "Name"]
-
                 showDataFrame(
                     "All", 
                     getDropOffLogAsc,
