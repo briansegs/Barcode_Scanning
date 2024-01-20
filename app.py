d0475bd1cd85787f092777cc3dcfd47ebe38cd6d
bsegers <briansegs@gmail.com>
2024-01-19 20:36:44 -0500

    docs: Remove TODOs

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ from data import mainMenueOptions
 # TODO: Spit out options into seperate files
 # TODO: Test features of app
 # TODO: Figure out how to give structure to newlines
-# TODO: Address Items Drop Off being broken
 # TODO: Switch order of login / scan Id first
-# TODO: Make dataframe for Items dropped off
 
 agent = login()
 
