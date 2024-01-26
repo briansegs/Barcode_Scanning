1e491f8fa53e38907df5c428e733afc35b9a957b
bsegers <briansegs@gmail.com>
2024-01-26 08:27:16 -0500

    refactor: Move variables

--- app/data/data.py
+++ app/data/data.py
@@ -1,11 +1,3 @@
 "App data"
 
 database = 'testDb.sqlite'
-
-itemColumns = ["ID", "Date", "Time", "Quantity",
-                "Item", "Agent", "Name", "location"]
-
-pendingDropOffColumns = ["Name", "Quantity", "Item_ID"]
-
-dropOffLogColumns = ["ID", "Date", "Time", "Quantity",
-                "Item", "Agent", "Name"]
